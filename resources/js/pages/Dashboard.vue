f3185d6a8bd745d6b365c133b79ceac632260d18
Ryan <ryan@dorn.studio>
2025-09-20 05:37:55 -0300

    test

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -10,13 +10,19 @@ const breadcrumbs: BreadcrumbItem[] = [
         href: '/dashboard',
     },
 ];
+
+const apiKey = import.meta.env.VITE_APP_API_KEY
+
 </script>
 
 <template>
     <Head title="Dashboard" />
 
+
+
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                {{ apiKey }}
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
